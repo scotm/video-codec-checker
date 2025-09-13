1adc04cbafb8a4deadcf8dff795f09c940bb8c54
Scott Macdonald <scott.scotm@gmail.com>
2025-09-13 21:42:42 +0100

    Tidy up whitespace.

--- video_codec_checker/config.py
+++ video_codec_checker/config.py
@@ -14,23 +14,23 @@ def load_yaml_config(config_file=None):
     else:
         # Cross-platform config location
         config_path = Path.home() / ".config" / "check-video-codecs.yml"
-    
+
     if config_path.exists():
         try:
             with open(config_path, 'r') as f:
                 config_data = yaml.safe_load(f)
                 return config_data if config_data else {}
-        except Exception as e:
+        except Exception:
             # We don't have access to sys.stderr here, so we'll just return empty dict
             return {}
-    
+
     return {}
 
 
 def load_env_config():
     """Load environment variables from .env file."""
     load_dotenv()
-    
+
     return {
         "output_file": os.environ.get("OUTPUT_FILE"),
         "scan_directory": os.environ.get("SCAN_DIRECTORY", "."),
